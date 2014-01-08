200184c2d347f0ab62b8bc6000a63a16faf7edfc
houst0nn <houstonn@gmail.com>
2014-01-08 11:58:01 +0100

    g2: Enable smooth-streaming video-playback
    Change-Id: I7e10ac5382f4ebbdae8056f75bb5040c88a1f258
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- g2.mk
+++ g2.mk
@@ -231,6 +231,9 @@ PRODUCT_PACKAGES += \
         loki_tool_static_g2 \
         recovery-transform.sh
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	mm.enable.smoothstreaming=true
+
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 ## These values are still too low and cause cache corruption. Use the hammerhead's
