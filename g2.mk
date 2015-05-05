varund7726/android_device_lge_g2-common

g2: Call common vendor folder
Change-Id: I94470566cad314ae139f9cacb5827adb0aea53a7

g2.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Call common vendor
+$(call inherit-product-if-exists, vendor/lge/g3-common/g3-common-vendor.mk)
+
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Overlays

SlimRoms/device_lge_g2-common

g2.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Call common vendor
+$(call inherit-product-if-exists, vendor/lge/g2-common/g2-common-vendor.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 